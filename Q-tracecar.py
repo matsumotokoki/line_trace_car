74821fdce3ee1e0546cbf637b14cce6dc6d2bee4
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2019-06-03 00:28:41 +0900

    fix plot system

--- Q-tracecar.py
+++ Q-tracecar.py
@@ -11,26 +11,26 @@ def get_state(car,s_state,v_state):
     return sum(np.array(state) * (np.array([1,2,4,8,16,(line_trace_car.car_order[1]-line_trace_car.car_order[0]+1)*16])))
 
 def decide_action(next_state,episode,q_table):
-    first_probability = 0.5
+    first_probability = 0.75
     epsilon = first_probability * (1/(episode+1))
     if epsilon <= np.random.uniform(0,1):
         next_action = np.argmax(q_table[next_state])
     else:
-        prob = sum(q_table[next_state]+1)
-        w = (q_table[next_state]+1) / prob
+        prob = sum(q_table[next_state]+100)
+        w = (q_table[next_state]+100) / prob
         next_action = np.random.choice(range(9) ,p=w) 
     return next_action
 
 def update_Qtable(q_table,state,action,reward,next_state):
-    gamma = 0.5
-    alpha = 0.4
+    gamma = 0.75
+    alpha = 0.5
     next_max_q = max(q_table[next_state])
     q_table[state,action] = (1 - alpha) * q_table[state,action] + alpha * (reward + gamma * next_max_q)
     return q_table
 
 def run():
-    max_episode = 1000000
-    step_by_episode = 10000
+    max_episode = 10000
+    step_by_episode = 5000
     goal_ave = 7
     review_num = 10
     reward_of_episode = 0
@@ -50,12 +50,15 @@ def run():
             v_state = car.step(action)[0]
             s_state = car.get_sense()
             done = car.checck_done(car.get_sense())
-            if sum(v_state) == 0:
+            if sum(v_state) < 1:
                 reward = -0.75
+            elif s_state[0] or s_state[3]:
+                reward = -0.25
             elif not done:
                 reward = 0.05
             else:
-                reward = done
+                # reward = done
+                reward = -10
 
             reward_of_episode += reward
             level = max(max(s_state), level)
@@ -65,25 +68,30 @@ def run():
             action = decide_action(next_state,episode,q_table)
             state = next_state
             if learining_is_done or episode % 1000 == 0:
+            #if learining_is_done:
                 car.plot_state()
                 field.plot_normal_field_line(plt)
                 car.path_plot(plt)
                 plt.draw()
-                plt.pause(0.0001)
+                plt.pause(0.00001)
             
             if done:
                 reward_ave = np.hstack((reward_ave[1:],level))
                 print("episode %5d, reward %5d, step %5d, x:%5d, y:%5d, level %d" %(episode+1,reward_of_episode,i+1,car.pos_x,car.pos_y,level))
                 if learining_is_done == 1 or episode % 1000 == 0:
+                #if learining_is_done == 1:
                     # field.plot_normal_field_line(plt)
                     # car.path_plot(plt)
                     # plt.show()
                     plt.close()
-                    plt.clf()
+                    # plt.clf()
                 break
 
 
         if (reward_ave.mean() >= goal_ave):
             print("Episode %d train agent successfuly!" % episode)
+            with open('./csv/file.csv', 'wt') as f:
+                writer = csv.writer(f)
+                writer.writerows(q_table)
             learining_is_done = 1
 run()
